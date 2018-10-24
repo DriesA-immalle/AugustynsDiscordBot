6cd589becf764508e21df648fbd6e20744f95227
Dries Augustyns <31651090+DriesA-immalle@users.noreply.github.com>
2018-10-24 18:53:19 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+
+TOKEN = 'NTA0NjYyMTU4MDg1MTkzNzI4.DrIYyg.Ib3TnRD88eUDqjDYXZgIIM06VuY'
+
+client = discord.Client()
+@client.event
+async def on_message(message):
+    if message.content.startswith("-ping"):
+        await client.send_message(message.channel, "Jesus Christ, yes I am here!")
+        print("Ping Command")
+
+@client.event
+async def on_member_join(member):
+    await client.send_message(discord.Object(id='504650972761554984'), 'Welcome **@' + str(member) + '** to the **Augustyns Graphics Server**! :sparkles:')
+    print("User Joined")
+
+@client.event
+async def on_ready():
+    print("Ready to Rumble!")
+client.run(TOKEN)
